40613da6a101d7e03acaa5b1c9162de8d3b539dd
marcusmariette <mariettemarcus@gmail.com>
2020-04-21 17:19:52 +1000

    Updated Dice and Text Output. Also cleaned up Matrix and Internal Documentation.

--- DiceRoller.java
+++ DiceRoller.java
@@ -1,48 +1,62 @@
+// Dice Roller Java Source Code
+// SWE20001 - Tuesday 2:30 Lab - Team 4
+
 import java.util.Random;
 import java.util.Scanner;
 
-// dice roller java source code
-// Also outputs the dice face as ASCII art
+// Dice Roller Class
 public class DiceRoller {
+    // A 3x3 Matrix of integers allows us to represent each of 9 sided dice face.
+    int[][][] faceConfig = { 
+        { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } },
+        { { 0, 0, 1 }, { 0, 0, 0 }, { 1, 0, 0 } },
+        { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } },
+        { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } },
+        { { 1, 0, 1 }, { 0, 1, 0 }, { 1, 0, 1 } },
+        { { 1, 0, 1 }, { 1, 0, 1 }, { 1, 0, 1 } },
+        { { 1, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 } },
+        { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } },
+        { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } } 
+    };
 
-    // This has printing information for all numbers
-    // For each number,3x3 matrix represents the face
-    int[][][] faceConfig = { { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } },
-                           { { 0, 0, 1 }, { 0, 0, 0 }, { 1, 0, 0 } },
-                           { { 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } },
-                           { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } },
-                           { { 1, 0, 1 }, { 0, 1, 0 }, { 1, 0, 1 } },
-                           { { 1, 0, 1 }, { 1, 0, 1 }, { 1, 0, 1 } },
-                           { { 1, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 } },
-                           { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } },
-                           { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } } };
-
+    // Main Function
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DiceRoller dice = new DiceRoller();
-        while (true) {
-            int result = dice.roll();
-            System.out.println("dice face value:" + result);
-            dice.draw(result);
 
-            System.out.println("Roll again? (type no to quit):");
+        // Until user specifies they want to exit the program
+        while (true) {
+            // Result of the dice roll
+            int result = dice.roll();
+
+            // Tells user the dice is rolling
+            System.out.println("Dice Rolling...");
+            // Draws the dice
+            dice.draw(result);
+            // Prints out the dice value
+            System.out.println("Dice Face Value: " + result);
+
+            System.out.println("Press [Enter] to Roll Again.");
+            System.out.println("Otherwise, Type 'No' to Exit: ");
+
+            // Checks for user "No" input.
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("n") ||
                     input.equalsIgnoreCase("no")) {
-                System.out.println("Bye!");
+                System.out.println("Exiting Program.");
                 scanner.close();
                 return;
             }
         }
     }
 
-    // Draw the dice face using ascii characters
+    // Draw the dice face using ASCII values and referring the our 3x3 matrix
     private void draw(int value) {
         int[][] displayVal = faceConfig[value - 1];
-        System.out.println("-----");
+        System.out.println("-------");
  
         for (int i = 0; i < 3; i++) {
-            System.out.print("|");
+            System.out.print("| ");
             for (int j = 0; j < 3; j++) {
                 if (displayVal[i][j] == 1) {
                     System.out.print("o");
@@ -50,9 +64,9 @@ public class DiceRoller {
                     System.out.print(" ");
                 }
             }
-            System.out.println("|");
+            System.out.println(" |");
         }
-        System.out.println("-----");
+        System.out.println("-------");
  
     }
 
